bd369b3970d6699c8c60b2537bfe743ba96fdbab
Tristan Rivoallan <tristan@rivoallan.net>
2012-03-10 17:23:00 +0100

    [setup] generated stub symfony applications : frontend & backend

---
+++ src/lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    net.daheardit-records.www
+ * @subpackage form
+ * @author     Constructions Incongrues <contact@constructions-incongrues.net> 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
