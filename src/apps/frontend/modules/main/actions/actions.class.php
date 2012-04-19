23f039d4bee89ecbf91a2899ca1a4457eba6117a
Tristan Rivoallan <trivoallan@trivoallan-Latitude-E6410.(none)>
2012-04-19 21:27:42 +0200

    [frontend] integrated static layout

---
+++ src/apps/frontend/modules/main/actions/actions.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * main actions.
+ *
+ * @package    net.daheardit-records.www
+ * @subpackage main
+ * @author     Constructions Incongrues <contact@constructions-incongrues.net>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class mainActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+ public function executeIndex(sfWebRequest $request)
+ {
+ 	// Select template
+ 	return sfView::SUCCESS;
+ }
+}
