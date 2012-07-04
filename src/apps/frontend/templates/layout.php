e7428158f22a4091c10427cf6b8b3f67898f43b2
Tristan Rivoallan <bertier@constructions-incongrues.net>
2012-07-04 23:52:40 +0200

    [frontend] analytics tracking code

--- src/apps/frontend/templates/layout.php
+++ src/apps/frontend/templates/layout.php
@@ -205,7 +205,7 @@
         <!-- end scripts-->
 
         <script>
-  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
+  var _gaq=[['_setAccount','UA-673133-1'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
   (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
     s.parentNode.insertBefore(g,s)}(document,'script'));
