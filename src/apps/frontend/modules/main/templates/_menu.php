8c30dd3933323b22527dc3257526e40ecadaf7cd
Bozoo <bozoorak@gmail.com>
2012-07-05 04:09:14 +0200

    infobulle donate

--- src/apps/frontend/modules/main/templates/_menu.php
+++ src/apps/frontend/modules/main/templates/_menu.php
@@ -24,7 +24,14 @@
             <li><a href="#informations"><?php echo __('Informations') ?></a></li>
             <li><a href="#contact"><?php echo __('Contact') ?></a></li>
             <li><a href="#links"><?php echo __('Liens') ?></a></li>
+
             <li><a href="http://www.musiques-incongrues.net/forum/labels/daheardit-records/"><?php echo __('Forum') ?></a></li>
-            <li class="header_nav_3_button_donate"><a href="#" class="donate paypal" data-paypalid="RDMSQRU34X5YS">&hearts; <?php echo __('Dons') ?> &hearts; </a></li>
+            <li class="header_nav_3_button_donate donate_infos"><a href="#" class="donate paypal" data-paypalid="RDMSQRU34X5YS">&hearts; <?php echo __('Dons') ?> &hearts;         
+              <span>
+
+                Depuis 2006, Da ! Heard It Records met à disposition du public des albums en téléchargement gratuit ou en support palpables à des prix raisonnables. Évidemment tout cela à un coût, ne serait-ce que pour payer les divers frais induits par la production d'un album.
+Vous pouvez nous aider en faisant un don d'argent au label.
+</li>
           </ul>
+
         </div><!-- end of header_nav_3_content -->
