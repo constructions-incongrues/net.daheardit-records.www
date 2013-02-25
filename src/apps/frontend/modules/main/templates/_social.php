f4a32450b97257d386301582e4d6c0074940d21c
Tristan Rivoallan <tristan@rivoallan.net>
2013-02-25 15:06:08 +0100

    [front/social] fixed youtube link, removed grooshark

--- src/apps/frontend/modules/main/templates/_social.php
+++ src/apps/frontend/modules/main/templates/_social.php
@@ -3,7 +3,7 @@
 <?php else: ?>
     <li><a href="<?php echo url_for('@feeds_posts?sf_culture='.$sf_request->getParameter('sf_culture', 'fr')) ?>"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/img-rss.png" /></a></li>
 <?php endif; ?>
-	<li><a href="http://grooveshark.com/#!/user/Da+Heard+It+Records/18463163"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/grooveshark.png" alt="Grooveshark"/></a></li>
+	<!-- <li><a href="http://grooveshark.com/#!/user/Da+Heard+It+Records/18463163"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/grooveshark.png" alt="Grooveshark"/></a></li> -->
 	<li><a href="http://freemusicarchive.org/label/Da__Heard_It_Records/"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/fma.png" alt="Free Music Archives"/></a></li>
 	<li><a href="http://www.last.fm/label/Da%2520!%2520Heard%2520It%2520Records?ac=da%20heard%20it%20records"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/img-lastfm.png" alt="Last Fm"/></a></li>
 	<li><a href="http://soundcloud.com/daheardit-records"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/soundcloud.png" /></a></li>
@@ -11,4 +11,4 @@
 	<li><a href="https://plus.google.com/102985904813984001814/posts"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/img-google.png" alt="Google +"/></a></li>
 	<li><a href="https://www.facebook.com/dahearditrecords"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/img-facebook.png" alt="Facebook"/></a></li>
 	<li><a href="http://vimeo.com/dahearditrecords"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/img-vimeo.png" alt="Vimeo"/></a></li>
-	<li><a href="www.youtube.com/dahearditrecords"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/youtube.png" /></a></li>
+	<li><a href="http://www.youtube.com/dahearditrecords"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/youtube.png" /></a></li>
