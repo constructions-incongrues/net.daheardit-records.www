563c7ce00b250a24c9396834418785a343a28c1d
Tristan Rivoallan <tristan@rivoallan.net>
2014-09-25 23:47:43 +0200

    [frontend/release] dons

--- src/apps/frontend/modules/release/templates/showSuccess.php
+++ src/apps/frontend/modules/release/templates/showSuccess.php
@@ -116,7 +116,7 @@
 
 
 <?php if (count($archives)): ?>
-    <h2 class="open_releases_title"><?php echo __('Télécharger l\'album') ?></h2>
+    <h2 class="open_releases_title"><?php echo __('Télécharger l\'album') ?> - Faire un <a href="#" class="donate paypal" data-paypalid="RDMSQRU34X5YS">&hearts; <?php echo __('don') ?> &hearts;</h2>
 
 <span style="margin-top:-25px;"> </span>
     <ul class="open_releases_download">
@@ -124,7 +124,7 @@
       <li><a class="release-download" data-dhr-release-slug="<?php echo $release['slug'] ?>" data-dhr-archive-format="<?php echo $archive['name'] ?>" href="<?php echo $sf_request->getRelativeUrlRoot() ?>/assets/releases/<?php echo $release['slug'] ?>/archives/<?php echo $archive['filename'] ?>"><?php echo strtoupper($archive['name']) ?></a></li>
   <?php endforeach ?>
     </ul>
- <p class="open_releases_download_licence">Ce(tte) œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/deed.fr">Licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 4.0 France</a>.</p>
+ <p class="open_releases_download_licence">Cette œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/deed.fr">Licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 4.0 France</a>.</p>
 <?php endif ?>
 
 <?php if ($release['is_available']): ?>
