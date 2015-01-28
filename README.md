097256af1b458397889b0150aa0016c044bb74a8
Tristan Rivoallan <tristan.rivoallan@aramisauto.com>
2015-01-28 22:48:29 +0100

    [vagrant] compatibilité Vagrant

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-https://www.codeship.io/projects/c7837670-43e7-0131-4635-2ec272a83621/status
+# Installation d'un environnement de développement
+
+```bash
+sudo apt-get install virtualbox resolvconf dnsmasq
+wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
+sudo dpkg -i vagrant_1.7.2_x86_64.deb
+vagrant plugin install vagrant-vbguest
+vagrant plugin install vagrant-share
+vagrant plugin install landrush
+vagrant up
+
+sudo sh -c 'echo "server=/vagrant.dev/127.0.0.1#10053" > /etc/dnsmasq.d/vagrant-landrush'
+sudo service dnsmasq restart
+```
+
+# Déploiement des sources
+
+```bash
+# Test
+ant deploy -Dprofile=jeroboam
+
+# Déploiement effectif
+ant deploy -Dprofile=jeroboam -Drsync.options=--delete-after
+```
---
+++ provision.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Mise à jour des dépots
+apt-get -qq update
+
+# Configuration de la timezone
+echo "Europe/Paris" > /etc/timezone 
+apt-get install -y tzdata
+dpkg-reconfigure -f noninteractive tzdata
+
+# Installation de Apache et PHP
+apt-get -y install libapache2-mod-php5 php5-cli
+
+# Installation de Git
+apt-get -y install git
+
+# Installation de MySQL
+echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
+apt-get install -y mysql-server
+
+# Installation de PhpMyAdmin
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password root" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password root" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password root" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
+apt-get install -y phpmyadmin
+
+# Création de la base de données
+mysql --defaults-file=/etc/mysql/debian.cnf -e "drop database if exists net_dahearditrecords_www"
+mysql --defaults-file=/etc/mysql/debian.cnf -e "create database net_dahearditrecords_www default charset utf8 collate utf8_general_ci"
+mysql --defaults-file=/etc/mysql/debian.cnf net_dahearditrecords_www < /vagrant/src/data/fixtures/net_dahearditrecords_www.dump.sql
+
+# Configuration du projet
+apt-get install -y ant 
+cd /vagrant
+git submodule update --init --recursive
+./composer.phar install --prefer-dist --no-progress
+ant configure build -Dprofile=vagrant
+/vagrant/src/symfony cache:clear
+
+# Mise à disposition du projet dans Apache
+ln -sf /vagrant/src/web/* /var/www/
+rm -f /var/www/index.html
+
+# Informations
+echo 
+echo -e "Le site est disponible à l'adresse : http://daheardit-records.vagrant.dev/frontend_dev.php"
+echo -e "PhpMyAdmin est disponible à l'adresse : http://daheardit-records.vagrant.dev/phpmyadmin/ (root / root)"
