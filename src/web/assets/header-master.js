8d51d421e577be112e40f0fd881e3c8963a24c3f
johan <bozoorak@gmail.com>
2015-03-10 22:27:45 +0100

    FUTUR HEADER DHR

--- src/web/assets/header-master.js
+++ src/web/assets/header-master.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('.header').click(function() {
-        window.location = 'https://www.youtube.com/watch?v=i7FEegisUYc';
+        window.location = 'http://www.daheardit-records.net/fr/discography/dhr-28';
     });
 });
