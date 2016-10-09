6513236e4b792a348717ddb6a689017ccf867978
johan <bozoorak@gmail.com>
2016-10-10 01:13:41 +0200

    Ok

--- src/apps/frontend/modules/main/templates/_social.php
+++ src/apps/frontend/modules/main/templates/_social.php
@@ -7,10 +7,10 @@
 	<li><a href="http://twitter.com/daheardit"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/img-twitter-w.png" alt="twitter"/></a></li>
 	<li><a href="http://freemusicarchive.org/label/Da__Heard_It_Records/"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/fma.png" alt="Free Music Archives"/></a></li>
  	<li><a href="http://vimeo.com/dahearditrecords"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/img-vimeo.png" alt="Vimeo"/></a></li>
+	<li><a href="http://soundcloud.com/daheardit-records"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/soundcloud.png" /></a></li>
 
 	<!-- <li><a href="http://grooveshark.com/#!/user/Da+Heard+It+Records/18463163"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/grooveshark.png" alt="Grooveshark"/></a></li> -->
 
 	<!-- <li><a href="http://www.last.fm/label/Da%2520!%2520Heard%2520It%2520Records?ac=da%20heard%20it%20records"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/img-lastfm.png" alt="Last Fm"/></a></li>
-	<li><a href="http://soundcloud.com/daheardit-records"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/soundcloud.png" /></a></li>
 	<li><a href="http://www.youtube.com/dahearditrecords"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/icon/youtube.png" /></a></li>
 -->
