23ada3a7f9363915bcd6438b8e6d23dd6365e2e4
Tristan Rivoallan <tristan@rivoallan.net>
2016-10-10 18:42:39 +0200

    [player] player Facebook *DE LUXE*

--- src/apps/frontend/modules/release/templates/showSuccess.mediarss.php
+++ src/apps/frontend/modules/release/templates/showSuccess.mediarss.php
@@ -6,16 +6,21 @@
 <?php endif; ?>
 <item>
     <!-- media:content is used as the clip URL -->
-    <media:content url="http://www.daheardit-records.net/assets/releases/<?php echo $release['slug'] ?>/tracks/<?php echo str_replace('-', '', $release['slug']) ?>_<?php echo $zero ?><?php echo $track['number'] ?>.mp3" type="audio/mpeg" />
+    <media:content
+        url="http://www.daheardit-records.net/assets/releases/<?php echo $release['slug'] ?>/tracks/<?php echo str_replace('-', '', $release['slug']) ?>_<?php echo $zero ?><?php echo $track['number'] ?>.mp3" 
+        type="audio/mpeg" />
 
     <link><?php echo url_for('@release_show?slug='.$release['slug'], true) ?></link>
 
     <!-- media:thumbnail is used to set a splash image into the playlist. -->
-    <media:thumbnail url="<?php echo sprintf($sf_request->getRelativeUrlRoot().'/assets/releases/%s/images/%s_1.png', $release['slug'], $release['slug']) ?>" />
+    <media:thumbnail url="<?php echo sprintf($sf_request->getUriPrefix().'/assets/releases/%s/images/%s_1.png', $release['slug'], $release['slug']) ?>" />
 
     <!-- you can set flowplayer specific properties for the thumbnail using a fp:thumbnail element -->
     <fp:thumbnail scaling="orig" />
 
-    <fp:clip autoPlay="true" captionUrl="<?php echo url_for('@release_track_show?sf_format=srt&slug='.$release['slug'].'&number='.$track['number']) ?>" />
+    <!-- Those are automatically maped to Flowplayer clip properties -->
+    <fp:clip
+        autoPlay="true"
+        captionUrl="<?php echo url_for(sprintf('@release_track_show?sf_format=srt&slug=%s&number=%d', $release['slug'], $track['number']), true) ?>" />
 </item>
 <?php endforeach ?>
