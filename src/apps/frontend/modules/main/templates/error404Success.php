df3c81d83d18b4aaacfbd9cef1416e82179969b1
Tristan Rivoallan <tristan@rivoallan.net>
2017-04-11 20:54:19 +0200

    [front] typo
    Signed-off-by: Tristan Rivoallan <tristan@rivoallan.net>

--- src/apps/frontend/modules/main/templates/error404Success.php
+++ src/apps/frontend/modules/main/templates/error404Success.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Da ! Heart It Records - T'es perdu ?</title>
+  <title>Da ! Heard It Records - T'es perdu ?</title>
   <style>
 html { 
   margin:0;
