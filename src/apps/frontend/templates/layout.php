74fdf510b2c226aa2e2e9bdb3d7c9951c6a9e351
Tristan Rivoallan <tristan@rivoallan.net>
2017-05-09 22:37:56 +0200

    [footer] newsletter + playlist

--- src/apps/frontend/templates/layout.php
+++ src/apps/frontend/templates/layout.php
@@ -119,7 +119,7 @@
       <div class="grid_12 category_infos">
          <div class="category_content">
           <h1 id="vids"><?php echo __('Vidéos') ?></h1>
-                      <span class="triangle"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/others/triangle-blanc.png" class="triangle" /></span>
+          <span class="triangle"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/others/triangle-blanc.png" class="triangle" /></span>
 
           <span class="infos_button_top"><a href="#top"><img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/button/top_black.png" alt="TOP"></a></span>
         </div><!-- end of category_contents -->
@@ -128,10 +128,8 @@
       <div class="grid_12 infos">
 
         <div class="infos_content">
-
-        
-
-<iframe width="960" height="540" src="https://www.youtube.com/embed/e8UoUJha3zw?list=PLgpmTifGJBPQGp_JLT572YE7wV1gWTEpN" frameborder="0" allowfullscreen></iframe>        </div><!-- end of .infos-->
+          <iframe width="960" height="540" src="https://www.youtube.com/embed/e8UoUJha3zw?list=PLgpmTifGJBPQGp_JLT572YE7wV1gWTEpN" frameborder="0" allowfullscreen></iframe>
+          </div><!-- end of .infos-->
       </div><!-- end of .infos content -->
 
 
@@ -148,58 +146,23 @@
           <div id="informations" class="grid_5 infos_descr">
 
           <!-- Begin MailChimp Signup Form -->
-
-<div id="mc_embed_signup">
-<form action="//daheardit-records.us15.list-manage.com/subscribe/post?u=a260251d9efb71cb8f1bffff4&amp;id=e77908b281" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-  <h1>Subscribe to our mailing list</h1>
-<!-- <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
- --><div class="mc-field-group">
-<!--   <label for="mce-EMAIL">Adresse Email  <span class="asterisk">*</span>
- --></label>
-  <input type="email" value="" name="EMAIL" placeholder="ajouter mon email" class="required email" id="mce-EMAIL">
-      <input type="submit" value="Ok" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-
-</div>
-  <div id="mce-responses" class="clear">
-    <div class="response" id="mce-error-response" style="display:none"></div>
-    <div class="response" id="mce-success-response" style="display:none"></div>
-  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-</form>
-</div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email'; /*
- * Translated default messages for the $ validation plugin.
- * Locale: FR
- */
-$.extend($.validator.messages, {
-        required: "Ce champ est requis.",
-        remote: "Veuillez remplir ce champ pour continuer.",
-        email: "Veuillez entrer une adresse email valide.",
-        url: "Veuillez entrer une URL valide.",
-        date: "Veuillez entrer une date valide.",
-        dateISO: "Veuillez entrer une date valide (ISO).",
-        number: "Veuillez entrer un nombre valide.",
-        digits: "Veuillez entrer (seulement) une valeur numérique.",
-        creditcard: "Veuillez entrer un numéro de carte de crédit valide.",
-        equalTo: "Veuillez entrer une nouvelle fois la même valeur.",
-        accept: "Veuillez entrer une valeur avec une extension valide.",
-        maxlength: $.validator.format("Veuillez ne pas entrer plus de {0} caractères."),
-        minlength: $.validator.format("Veuillez entrer au moins {0} caractères."),
-        rangelength: $.validator.format("Veuillez entrer entre {0} et {1} caractères."),
-        range: $.validator.format("Veuillez entrer une valeur entre {0} et {1}."),
-        max: $.validator.format("Veuillez entrer une valeur inférieure ou égale à {0}."),
-        min: $.validator.format("Veuillez entrer une valeur supérieure ou égale à {0}.")
-});}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-<!--End mc_embed_signup-->
+        <div id="mc_embed_signup" name="#newsletter">
+          <form action="//daheardit-records.us15.list-manage.com/subscribe/post?u=a260251d9efb71cb8f1bffff4&amp;id=e77908b281" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <h1 name="newsletter"><?php echo __("S'inscrire à la liste d'informations") ?></h1>
+              <div class="mc-field-group">
+                <input type="email" value="" name="EMAIL" placeholder="Email" class="required email" id="mce-EMAIL">
+                <input type="submit" value="OK" name="subscribe" id="mc-embedded-subscribe" class="button">
+              </div>
+            </div><!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          </form>
+        </div>
 
           <h1 id="links"><?php echo __('Informations') ?></h1>
 
             <p>
             <?php include_component('content', 'show', array('block' => 'informations')) ?>
             </p>
-
-           
-
 
     <div class="grid_5 footer_links_1">
 
@@ -223,7 +186,7 @@ $.extend($.validator.messages, {
             <ul class="footer_nav_1">
 <?php include_partial('main/social') ?>
             </ul>
-            
+
 
             <h2><?php echo __('Adresse') ?></h2>
 
@@ -275,8 +238,6 @@ $.extend($.validator.messages, {
           </div><!-- end of footer -->
 
         </div><!-- end of container 12s -->
-
-
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
