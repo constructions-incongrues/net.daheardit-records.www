ee4d35d77cc40033b2337d67fcb80191f3ccde95
Johan Mailleret <bozoorak@gmail.com>
2018-09-05 01:25:13 +0200

    test debug ancre google chrome

--- src/apps/frontend/modules/main/templates/_menu.php
+++ src/apps/frontend/modules/main/templates/_menu.php
@@ -6,7 +6,7 @@
         <div class="header_nav_3_content">
           <ul id="menu-main" class="header_nav_3_content_list">
             <li class="header_nav_3_content_logo"><a href="<?php echo url_for('@homepage') ?>"> <img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/logo/logo-dhr.png" alt="logo" /></a></li>
-            <li><a href="#discography"><?php echo __('Discographie') ?></a></li>
+            <li><a name="#discography"><?php echo __('Discographie') ?></a></li>
             <li><a href="#artists"><?php echo __('Artistes') ?></a></li>
             <li><a href="#artwork_artists"><?php echo __('Illustrations') ?></a></li>
             <li> <a href="#videos"><?php echo __('Vidéos') ?></a></li>
