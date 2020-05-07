7b3927b1e0b39069d3544898689f8deb866868d7
Johan Mailleret <bozoorak@gmail.com>
2020-05-07 16:01:31 +0200

    hop

--- src/apps/frontend/modules/radio/templates/indexSuccess.php
+++ src/apps/frontend/modules/radio/templates/indexSuccess.php
@@ -23,12 +23,7 @@
 <body>
 
 
-
-
-      <div class="clear"></div><!-- end of clear -->
-
       <div class="grid_12 header_nav_3">
-        <div class="header_nav_3_content">
           <ul id="menu-main" class="header_nav_3_content_list">
             <li class="header_nav_3_content_logo"><a href="<?php echo url_for('@homepage') ?>"> <img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/logo/logo-dhr.png" alt="logo" /></a></li>
             <li><a href="https://www.daheardit-records.net/fr/#discography"><?php echo __('Discographie') ?></a></li>
@@ -36,35 +31,55 @@
             <li><a href="https://www.daheardit-records.net/fr/#artwork_artists"><?php echo __('Illustrations') ?></a></li>
             <li><a href="https://www.daheardit-records.net/fr/#informations"><?php echo __('Informations') ?></a></li>
      
-            <li class="header_nav_3_button_donate donate_infos"><a href="https://www.helloasso.com/associations/constructions-incongrues" class="donate"><?php echo __('Dons') ?>
+            <li class="header_nav_3_button_donate donate_infos"><a href="https://www.helloasso.com/associations/constructions-incongrues" class="donate"><?php echo __('Dons') ?></a>
 </li>
           </ul>
 
         </div><!-- end of header_nav_3_content -->
 
-<div class="category_content">
 
-<h2 class=""></a> </h2>
+<h2>.</h2>
+<h1> Release Party </h1>
+<p> Wankers United - Solo Romance (digital & tape)  <br /><a href="https://www.daheardit-records.net/fr/discography/dhr-46">https://www.daheardit-records.net/fr/discography/dhr-46</a>
 
-<h2 class="">En écoute actuellement sur la radio : </h2>
-
-<p> 
+<h2 class="">Listen : <a href="https://p-node.org/">P-NODE</a> / <a href="http://stream.p-node.org/daheardit.m3u">M3U</a></p>
+</h2>
 
 <audio controls src="http://stream.p-node.org/daheardit"></audio>
 
 
-
-<h2 class="">Discutons :)</h2>
-
-<p>Le streaming est géré par l'excellent radio <a href="https://p-node.org/">https://p-node.org/</a></p>
+<h2 class="">Line Up </h2>
 
 <p>
+<a href="https://www.daheardit-records.net/en/artist/bill-vortex#artist">Bill Vortex</a> (Live / Marseille) <em>Electro / Braindance / Aïoli</em> <br />
+IW¥N aka <a href="https://www.daheardit-records.net/en/artist/eat-rabbit#artist">Eat Rabbit</a> (Live / Caluire Sur Cuire) <em>Noise / Ambiant / Techno </em><br />
+<a href="http://www.glafouk.com">Glafouk</a> (Dj-set / Paris) <em>SID / Commodore / 64 </em><br />
+<a href="https://www.daheardit-records.net/en/artist/wankers-united#artist">Wankers United</a>  (Live / Saint Ouen) <em>Funk / Synthétiseur / Kimchi </em><br />
+<a href="https://www.daheardit-records.net/en/artist/vicnet#artist">Vicnet</a> (Live / Paris - Saint-Mandé) <em>Funk / JV1080 / Midi </em><br />
+<a href="https://www.ouiedire.net/emission/ailleurs-227">Santa Barbera</a> (Dj-set / Vitre-Sur-Seine) <em>Hiphouse / Electro / Funkin<em><br />
+</p>
+
+<h2 class="">Talk :</h2>
+
+<h2 class="">Zoom  </h2>
+<p>
+ID de réunion : 882 1665 3714<br />
+Mot de passe : 745205<br />
+<a href="https://us02web.zoom.us/j/88216653714?pwd=Y3RZOVhjd1FoNms2VDVub1NjdWVIUT09">https://us02web.zoom.us/j/88216653714?pwd=Y3RZOVhjd1FoNms2VDVub1NjdWVIUT09</a><br />
+
+</p>
+
+<img src="/frontend/pics/radio/flyer_8_may.png" alt="flyer" />
+
+<!-- <p>
     <?php include_component('content', 'show', array('block' => 'radio_onair')) ?>
 </p>
 
 <p>
     <?php include_component('content', 'show', array('block' => 'radio_presentation')) ?>
 </p>
+ -->
+
 </div>
 </div>
 
