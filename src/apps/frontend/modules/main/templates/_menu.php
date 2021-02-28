267195d95e30804fa515cb71fb89c3402f7bd5b0
Johan <bozoorak@gmail.com>
2021-03-01 00:13:15 +0100

    hop

--- src/apps/frontend/modules/main/templates/_menu.php
+++ src/apps/frontend/modules/main/templates/_menu.php
@@ -6,14 +6,14 @@
         <div class="header_nav_3_content">
           <ul id="menu-main" class="header_nav_3_content_list">
             <li class="header_nav_3_content_logo"><a href="<?php echo url_for('@homepage') ?>"> <img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/logo/logo-dhr.png" alt="logo" /></a></li>
-            <li><a href="#discography"><?php echo __('Discographie') ?></a></li>
-            <li><a href="#artists"><?php echo __('Artistes') ?></a></li>
-            <li><a href="#artwork_artists"><?php echo __('Illustrations') ?></a></li>
+            <li><a href="#discography"><?php echo __('Releases') ?></a></li>
+            <li><a href="#artists"><?php echo __('Artists') ?></a></li>
+            <li><a href="#artwork_artists"><?php echo __('Illustrator') ?></a></li>
             <li><a href="https://dahearditrecords.bandcamp.com/"><?php echo __('Bandcamp') ?></a></li>
 
             <!-- <li> <a href="http://www.daheardit-records.net/radio"><?php echo __('Radio') ?></a></li> -->
-            <li> <a href="#videos"><?php echo __('Documentaire') ?></a></li>
-            <li><a href="#informations"><?php echo __('Infos') ?></a></li>
+            <li> <a href="#videos"><?php echo __('Documentary') ?></a></li>
+            <li><a href="#informations"><?php echo __('About') ?></a></li>
             <li><a href="<?php echo url_for('@homepage_localized?sf_culture=fr') ?>">FR </a> / <a href="<?php echo url_for('@homepage_localized?sf_culture=en') ?>">EN</a></li>
             <!--<li><a href="http://www.musiques-incongrues.net/forum/labels/daheardit-records/"><?php echo __('Forum') ?></a></li> -->
             <li class="header_nav_3_button_donate donate_infos"><a href="https://www.helloasso.com/associations/constructions-incongrues" class="donate"><?php echo __('Dons') ?>
