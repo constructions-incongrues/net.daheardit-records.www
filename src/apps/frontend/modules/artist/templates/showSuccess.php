1ad503c7c42807fa598bfb4d275d841fe44f7444
Johan <bozoorak@gmail.com>
2021-12-25 18:13:05 +0100

    d

--- src/apps/frontend/modules/artist/templates/showSuccess.php
+++ src/apps/frontend/modules/artist/templates/showSuccess.php
@@ -10,12 +10,7 @@
   
   <div class="grid_12">
   <h1 class="open_artists_name"><?php echo $artist['name'] ?></h1>
-  <p class="open_artists_bio">
-      <?php echo nl2br($artist['Translation'][$sf_user->getCulture()]['presentation']) ?>
-      <?php if ($artist['url']): ?>
-          <br><br><a href="<?php echo $artist['url'] ?>"><?php echo $artist['url'] ?></a>
-      <?php endif ?>
-    </p>
+
     <ul class="open_artists_button">
 <?php if ($previousArtist): ?>
       <li class="open_releases_button_left"> <a class="previous" href="<?php echo url_for('@artist_show?slug='.$previousArtist['slug'].'#artist') ?>" data-pjax="#content_async"> <img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/button/hack.png" title="<?php echo $previousArtist['name'] ?>" /> </a></li>
@@ -26,6 +21,27 @@
 <?php endif; ?>
       <li class="open_releases_button_close"> <a class="close" href="<?php echo url_for('@homepage#artists') ?>" data-close="#content_async" title="<?php echo __('Retourner à la liste des artistes') ?>"> <img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/button/hack.png" alt="Close" /> </a></li>
     </ul>
+    <p class="open_artists_bio">
+      <?php echo nl2br($artist['Translation'][$sf_user->getCulture()]['presentation']) ?>
+      <?php if ($artist['url']): ?>
+          <br><br><a href="<?php echo $artist['url'] ?>"><?php echo $artist['url'] ?></a>
+      <?php endif ?>
+    </p>
+
+    <?php foreach ($artist['releases'] as $release): ?>
+       <div class="open_artists_listing_releases ">
+           <span>
+                <a data-pjax="#content_async" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>">
+                    <span class="calque_releases"></span>
+
+                    <img width="220px" height="220px" src="<?php echo $sf_request->getRelativeUrlRoot() ?>/assets/releases/<?php echo $release['slug'] ?>/<?php echo $release['slug'] ?>_300x300.png" alt=""/>
+                    <h1 style="margin-left:-5px;"><a data-pjax="#content_async" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>"><?php echo $release['sku'] ?></a></h1>
+                    <h2 style="margin-bottom:0px;"><a data-pjax="#content_async" title="<?php echo sprintf('%s - %s', $artist['name'], $release['title']) ?>" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>"><?php echo truncate_text(sprintf('%s', $artist['name'], $release['title']), 35) ?></a></h2>
+                </a>
+        </span>
+    </div>
+
+<?php endforeach; ?>
   </div>
 
   
@@ -49,20 +65,7 @@
    
 
 <div style="width:600px;">
- <?php foreach ($artist['releases'] as $release): ?>
-       <div class="open_artists_listing_releases ">
-           <span>
-                <a data-pjax="#content_async" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>">
-                    <span class="calque_releases"></span>
 
-                    <img width="220px" height="220px" src="<?php echo $sf_request->getRelativeUrlRoot() ?>/assets/releases/<?php echo $release['slug'] ?>/<?php echo $release['slug'] ?>_300x300.png" alt=""/>
-                    <h1 style="margin-left:-5px;"><a data-pjax="#content_async" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>"><?php echo $release['sku'] ?></a></h1>
-                    <h2 style="margin-bottom:0px;"><a data-pjax="#content_async" title="<?php echo sprintf('%s - %s', $artist['name'], $release['title']) ?>" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>"><?php echo truncate_text(sprintf('%s', $artist['name'], $release['title']), 35) ?></a></h2>
-                </a>
-        </span>
-    </div>
-
-<?php endforeach; ?>
 </div>
     </div><!-- end of grid_6 -->
   </div><!-- enf of release_content -->
