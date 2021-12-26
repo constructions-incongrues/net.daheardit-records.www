a11c6626145e5d6cf948992abdece2051a299f99
Johan <bozoorak@gmail.com>
2021-12-26 15:24:48 +0100

    s

--- src/apps/frontend/modules/release/templates/showSuccess.php
+++ src/apps/frontend/modules/release/templates/showSuccess.php
@@ -23,10 +23,10 @@
 <div class="grid_12 open_releases">
   <div class="open_releases_content">
 
+
   <div class="grid_12">
 
-
-     <ul class="open_releases_button">
+  <ul class="open_releases_button">
 <?php if ($previousRelease): ?>
       <li class="open_releases_button_left"> <a class="previous" href="<?php echo url_for('@release_show?slug='.$previousRelease['slug'].'#release') ?>" data-pjax="#content_async"> <img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/button/hack.png" title="<?php echo sprintf('[%s] %s', $previousRelease['sku'], $previousRelease['title' ]) ?>" /> </a></li>
 <?php endif; ?>
@@ -35,11 +35,6 @@
 <?php endif; ?>
       <li class="open_releases_button_close"> <a class="close" href="<?php echo url_for('@homepage#discography') ?>" data-close="#content_async" title="<?php echo __('Retourner à la liste des sorties') ?>"> <img src="<?php echo $sf_request->getRelativeUrlRoot() ?>/frontend/pics/button/hack.png" alt="Close" /> </a></li>
     </ul>
-  </div>
-
-  <div class="grid_12">
-
-
 <div class="presentation">
       <h2 class="open_releases_artist">
       <span class="date_release"></span>
