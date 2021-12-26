5efe497105fd1c04752f3beecb00b970c5d945ab
Johan <bozoorak@gmail.com>
2021-12-26 13:50:44 +0100

    d

--- src/apps/frontend/modules/release/templates/showSuccess.php
+++ src/apps/frontend/modules/release/templates/showSuccess.php
@@ -43,10 +43,10 @@
 
 <div class="presentation">
       <h2 class="open_releases_artist">
-      <span class="date_release"></span>
       <span class="sku"><?php echo $release['sku'] ?> /
-      <?php echo $release['released_at'] ?> </span> 
       <span><a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons BY-NC-ND 4.0</a></span>
+
+      <?php echo $release['released_at'] ?> </span> 
 
 <a href="<?php echo url_for(sprintf('@artist_show?slug=%s#artist', $release['Artist']['slug'])) ?>"><br /><?php echo $release['Artist']['name'] ?></a>  <?php echo $release['title'] ?> </h2>
 <?php echo nl2br(html_entity_decode($release['Translation'][$sf_user->getCulture()]['presentation'])) ?>
