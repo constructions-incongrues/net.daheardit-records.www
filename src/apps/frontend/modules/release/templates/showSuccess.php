066de1f56bbf2f61d79788f9bdc3cf97f5a56c1a
Johan <bozoorak@gmail.com>
2021-12-26 13:38:31 +0100

    d

--- src/apps/frontend/modules/release/templates/showSuccess.php
+++ src/apps/frontend/modules/release/templates/showSuccess.php
@@ -50,9 +50,9 @@
 <?php echo nl2br(html_entity_decode($release['Translation'][$sf_user->getCulture()]['presentation'])) ?>
 <?php if (count($release['press'])): ?>
  
-
+<span> They are talking : </span>
 <?php foreach ($release['press'] as $link): ?>
-      <span class="reviews"> Press : <a href="<?php echo $link['url'] ?>"><?php echo basename($link['title']) ?></a> </span>
+      <span class="reviews"> <a href="<?php echo $link['url'] ?>"><?php echo basename($link['title']) ?></a> </span>
   <?php endforeach ?>
 <?php endif ?>
   </div>
