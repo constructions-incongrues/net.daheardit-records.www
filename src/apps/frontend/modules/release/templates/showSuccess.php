ab5cdef8a4f259e235cd206c40e57e9a6f048215
Tristan Rivoallan <tristan@rivoallan.net>
2022-01-19 21:15:53 +0100

    release.licence

--- src/apps/frontend/modules/release/templates/showSuccess.php
+++ src/apps/frontend/modules/release/templates/showSuccess.php
@@ -38,7 +38,7 @@
 <div class="presentation">
       <h2 class="open_releases_artist">
       <span class="date_release"></span>
-      <span class="sku"><?php echo $release['sku'] ?> / <?php echo $release['licence'] ?>  /
+      <span class="sku"><?php echo $release['sku'] ?> / <?php echo html_entity_decode($release['licence']) ?>  /
 
       <?php echo $release['released_at'] ?> </span> 
 <a href="<?php echo url_for(sprintf('@artist_show?slug=%s#artist', $release['Artist']['slug'])) ?>"><br /><?php echo $release['Artist']['name'] ?></a>  <?php echo $release['title'] ?> </h2>
