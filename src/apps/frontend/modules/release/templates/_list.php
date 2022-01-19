90a9b14f959468b49a20efc60dbcf30339a09708
Johan <bozoorak@gmail.com>
2022-01-19 20:41:58 +0100

    jop

--- src/apps/frontend/modules/release/templates/_list.php
+++ src/apps/frontend/modules/release/templates/_list.php
@@ -12,7 +12,7 @@
             <span class="releases_discography_last_release"><?php echo __('Nouveau') ?></span>  
   <?php endif; ?>
           <img height="220px" width="220px" src="<?php echo $release['image'] ?>" />
-            <h1><a data-pjax="#content_async" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>"><?php echo $release['sku'] ?> <?php echo $release['Artist']['name'] ?></a></h1>
+            <h1><a data-pjax="#content_async" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>"> <?php echo $release['Artist']['name'] ?></a></h1>
             <h2><a data-pjax="#content_async" title="<?php echo $release['title'] ?>" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>"><?php echo truncate_text($release['title'], 35) ?></a></h2>
           </div><!-- end of grid_3 releases_discography_number  -->
 <?php endfor; ?>
