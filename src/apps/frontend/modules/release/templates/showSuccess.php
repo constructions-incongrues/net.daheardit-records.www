5866e1972f95ba442a7dcb5ccabab9b0ef8f8d6a
Johan <bozoorak@gmail.com>
2022-01-19 18:43:47 +0100

    d

--- src/apps/frontend/modules/release/templates/showSuccess.php
+++ src/apps/frontend/modules/release/templates/showSuccess.php
@@ -109,13 +109,14 @@
 
 Artwork by <a href="<?php echo url_for(sprintf('@artwork_artist_show?slug=%s#artwork_artist', $release['ArtworkArtist']['slug'])) ?>"><?php echo $release['ArtworkArtist']['name'] ?></a><br>
 <?php echo nl2br(html_entity_decode($release['credits'])) ?>
-</p>
 <?php if (count($release['press'])): ?>
 <span>reviews :</span>
 <?php foreach ($release['press'] as $link): ?>
         <span class="reviews"> <a href="<?php echo $link['url'] ?>"><?php echo basename($link['title']) ?></a> / </span>
   <?php endforeach ?>
 <?php endif ?>
+</p>
+
 <?php endif ?>
 
     </p>
