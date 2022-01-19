231a52dc59d69a4a08a559c3c7c7c91e9b831e0c
Johan <bozoorak@gmail.com>
2022-01-20 00:17:22 +0100

    d

--- src/apps/frontend/modules/artist/templates/showSuccess.php
+++ src/apps/frontend/modules/artist/templates/showSuccess.php
@@ -62,7 +62,7 @@
 <div>
       <a data-pjax="#content_async" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>">
 
-<img width="220px" height="220px" src="<?php echo $sf_request->getRelativeUrlRoot() ?>/assets/releases/<?php echo $release['slug'] ?>/<?php echo $release['slug'] ?>_300x300.png" alt=""/>
+<img width="220px" height="220px" src="<?php echo $sf_request->getRelativeUrlRoot() ?>/assets/releases/<?php echo $release['slug'] ?>/<?php echo $release['slug'] ?>_300x300.png" alt=""/></a>
     </div>    
 </div>
     </div>
