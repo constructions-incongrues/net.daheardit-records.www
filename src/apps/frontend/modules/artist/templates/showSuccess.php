f1fc7093cbd27523258fe5c9586ee45f4fdf8bfa
Johan <bozoorak@gmail.com>
2022-01-19 23:54:09 +0100

    d

--- src/apps/frontend/modules/artist/templates/showSuccess.php
+++ src/apps/frontend/modules/artist/templates/showSuccess.php
@@ -55,11 +55,13 @@
 
   
 
-    <div class="open_artists_listing_releases "> 
+    <div class="open_artists_listing_releases "> <div><a data-pjax="#content_async" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>">
+                <img width="220px" height="220px" src="<?php echo $sf_request->getRelativeUrlRoot() ?>/assets/releases/<?php echo $release['slug'] ?>/<?php echo $release['slug'] ?>_300x300.png" alt=""/>
+
+    </div>
     <?php foreach ($artist['releases'] as $release): ?>
       
-                <div><a data-pjax="#content_async" href="<?php echo url_for(sprintf('@release_show?slug=%s#release', $release['slug'])) ?>">
-    </div>
+                
     </div>
     </div>
     </div>
