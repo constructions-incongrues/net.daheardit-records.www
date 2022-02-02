6ac6e85fcde2f1c5b44bcb1d8d49f799285a8a29
Johan <bozoorak@gmail.com>
2022-02-02 21:10:53 +0100

    s

--- src/apps/frontend/modules/release/templates/showSuccess.php
+++ src/apps/frontend/modules/release/templates/showSuccess.php
@@ -70,6 +70,8 @@
 <a href="<?php echo url_for(sprintf('@artist_show?slug=%s#artist', $release['Artist']['slug'])) ?>"><br /><?php echo $release['Artist']['name'] ?></a>  <?php echo $release['title'] ?> </h2>
 <img id="carousel-current" height="30%" width="30%" style="float:left;padding:9px 20px 0px 0px" src="<?php echo sprintf('//%s/thumbnail/_/500/500/crop/best%s', sfConfig::get('app_api_url_root'), $release['artworks'][0]) ?>" />
 <p class="text-bio"><?php echo nl2br(html_entity_decode($release['Translation'][$sf_user->getCulture()]['presentation'])) ?>
+<?php echo nl2br(html_entity_decode($release['writers'])) ?> 
+
 </p>
  </div>
     <div class="grid_12 open_releases_artwork">
