19308b978b9186013f55c2a0ca53f9129e9b4c1d
Johan <bozoorak@gmail.com>
2022-02-02 20:33:57 +0100

    s

--- src/apps/frontend/modules/release/templates/showSuccess.php
+++ src/apps/frontend/modules/release/templates/showSuccess.php
@@ -38,9 +38,7 @@
 <div class="presentation">
       <h2 class="open_releases_artist">
       <span class="date_release"></span>
-      <span class="sku"><?php echo $release['sku'] ?> / <?php echo html_entity_decode($release['licence']) ?>  /
-
-      <?php echo $release['released_at'] ?> / 
+      <span class="sku"><?php echo $release['sku'] ?> / 
     
 <?php if ($release['is_available']): ?>
     <!-- <span style="font-weight: bold;">Acheter :</span> -->
@@ -64,7 +62,10 @@
 
   <?php foreach ($release['streaming'] as $link): ?>
       <a class="release-download" href="<?php echo $link['url'] ?>"><?php echo strtolower(basename($link['title'])) ?></a> -
-  <?php endforeach ?>
+  <?php endforeach ?> /
+  <?php echo html_entity_decode($release['licence']) ?>  /
+
+      <?php echo $release['released_at'] ?> / 
   </p>
 <?php endif ?></span> 
 <a href="<?php echo url_for(sprintf('@artist_show?slug=%s#artist', $release['Artist']['slug'])) ?>"><br /><?php echo $release['Artist']['name'] ?></a>  <?php echo $release['title'] ?> </h2>
