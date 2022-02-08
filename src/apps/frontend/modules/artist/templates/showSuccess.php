915a66df95900b0cda967ba0e62680dc9948f93a
Johan <bozoorak@gmail.com>
2022-02-08 21:59:58 +0100

    hop

--- src/apps/frontend/modules/artist/templates/showSuccess.php
+++ src/apps/frontend/modules/artist/templates/showSuccess.php
@@ -40,13 +40,15 @@
 
   <div class="presentation">
 
-    <h1 class="open_artists_name"><?php echo $artist['name'] ?>     <?php if ($artist['url']): ?>      <a href="<?php echo $artist['url'] ?>">(http://)</a></h1>
+  <h1 class="open_artists_name"><?php echo $artist['name'] ?>     <?php if ($artist['url']): ?>      <a href="<?php echo $artist['url'] ?>">(http://)</a></h1>
 
     <p class="grid_8 open_artists_bio">
     <img height="30%" width="30%" style="float:left;padding:10px 20px 0px 0px" src="<?php echo $sf_request->getRelativeUrlRoot() ?>/assets/artists/<?php echo $artist['slug'] ?>/<?php echo $artist['slug'] ?>_300x300.jpg" alt="" /> 
     <span class="text-bio">
       <?php echo nl2br($artist['Translation'][$sf_user->getCulture()]['presentation']) ?>
-  
+      <?php if ($artist['url']): ?>
+      <a href="<?php echo $artist['url'] ?>">(web)
+      </a>
       <?php endif ?>
     
       </span>
