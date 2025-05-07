9109a126504da91b8e5d0eb7376e5dfc6674ed1b
Tristan Rivoallan <tristan@rivoallan.net>
2025-05-07 04:25:50 +0200

    refactor: simplify image source URLs in showSuccess.php

--- src/apps/frontend/modules/release/templates/showSuccess.php
+++ src/apps/frontend/modules/release/templates/showSuccess.php
@@ -72,22 +72,22 @@
   </span>
 <a href="<?php echo url_for(sprintf('@artist_show?slug=%s#artist', $release['Artist']['slug'])) ?>"><br /><?php echo $release['Artist']['name'] ?></a>  <?php echo $release['title'] ?> </h2>
 <div class="grid_12 open_releases_infos show-mobile">
-<img class="show-mobile"id="carousel-current" height="100%" width="100%" src="<?php echo sprintf('//%s/thumbnail/_/500/500/crop/best%s', sfConfig::get('app_api_url_root'), $release['artworks'][0]) ?>" />
+<img class="show-mobile" id="carousel-current" height="100%" width="100%" src="<?php echo $release['artworks'][0] ?>" />
 
      <br /> <?php echo html_entity_decode($release['player_code']) ?>
 
   </div>
 
-  <img class="hidden-mobile"id="carousel-current" height="30%" width="30%" style="float:left;padding:9px 20px 0px 0px" src="<?php echo sprintf('//%s/thumbnail/_/500/500/crop/best%s', sfConfig::get('app_api_url_root'), $release['artworks'][0]) ?>" />
+  <img class="hidden-mobile" id="carousel-current" height="30%" width="30%" style="float:left;padding:9px 20px 0px 0px" src="<?php echo $release['artworks'][0] ?>" />
 <p class="text-bio"><?php echo nl2br(html_entity_decode($release['Translation'][$sf_user->getCulture()]['presentation'])) ?>
 <?php echo nl2br(html_entity_decode($release['writers'])) ?> 
 
 </p>
  </div>
     <div class="grid_12 open_releases_artwork">
-      <!-- <img id="carousel-current" height="460px" width="460px" src="<?php echo sprintf('//%s/thumbnail/_/500/500/crop/best%s', sfConfig::get('app_api_url_root'), $release['artworks'][0]) ?>" /> -->
+      <!-- <img id="carousel-current" height="460px" width="460px" src="<?php echo $release['artworks'][0] ?>" /> -->
 <?php for ($i = 1; $i < count($release['artworks']); $i++): ?>
-      <!-- <img height="460px" width="460px" src="<?php echo sprintf('//%s/thumbnail/_/500/500/crop/best%s', sfConfig::get('app_api_url_root'), $release['artworks'][$i]) ?>" /> -->
+      <!-- <img height="460px" width="460px" src="<?php echo $release['artworks'][$i] ?>" /> -->
 <?php endfor ?>
 
 
